a5dba29e6b5f6f4b3db819616503cd2bd5203788
developer <bosan007@protonmail.com>
2019-12-19 08:26:27 +0400

    add count index for query

--- application/models/ApiModel.php
+++ application/models/ApiModel.php
@@ -66,7 +66,7 @@ class ApiModel extends CI_Model
         }
 
         $result = [];
-
+        $i = 0;
         while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
             $documents['Company']  = $row['Company'];
             $documents['JobNum']  = $row['JobNum'];
