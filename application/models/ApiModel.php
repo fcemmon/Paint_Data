df56b491f599a03869bc99740964b350bdd5b4ff
developer <bosan007@protonmail.com>
2019-12-19 08:17:32 +0400

    update table name in query

--- application/models/ApiModel.php
+++ application/models/ApiModel.php
@@ -38,7 +38,6 @@ class ApiModel extends CI_Model
 		}
 
         sqlsrv_close($conn);
-
     }
     
     //Search Data on the basis of RACID,JOB,PRINT NUMBER
@@ -58,61 +57,37 @@ class ApiModel extends CI_Model
 
         $stmt = sqlsrv_query($conn, $sql);
 
-        // Check connection
-        if ($stmt) {
-            echo "Connect Database";
-        } else {
+        if (!$stmt) {
+            generateServerResponse(F, FAIL);
             die( print_r( sqlsrv_errors(), true));
-            echo "Query is failed";
+            sqlsrv_free_stmt( $stmt);
+            sqlsrv_close($conn);
+            return;
         }
-        // // Check connection
-        // if ($mysqli -> connect_errno) {
-        //     $mysqli->close();
-        //     generateServerResponse(F, FAIL);
-        // }
-        // else
-        // {
-        //     $sql = "SELECT * from PaintFirmJobs_TEST where RackStyle = '".$request['rackId']."' and JobNum = '".$request['job']."' and PaintMethod = '".$request['paintNumber']."'";
+
+        while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+            $documents['Company']  = $row['Company'];
+            $documents['JobNum']  = $row['JobNum'];
+            $documents['ProdQty']  = $row['ProdQty'];
+            $documents['PaintMethod']  = $row['PaintMethod'];
+            $documents['RackStyle']  = $row['RackStyle'];
+            $documents['VehicleFamily']  = $row['VehicleFamily'];
+            $documents['PartNum']  = $row['PartNum'];
+            $documents['PartDescription']  = $row['PartDescription'];
+            $documents['DueDate']  = $row['DueDate'];
+            $documents['JobClosed']  = $row['JobClosed'];
+            $documents['JobComplete']  = $row['JobComplete'];
+            $documents['DateCreated']  = $row['DateCreated'];
+            $documents['DateModified']  = $row['DateModified'];
             
-        //     //fetch data as per above query
-        //     $rows  = $mysqli->query($sql);
-            
-        //     // it return number of rows in the table. 
-        //     $row = mysqli_num_rows($rows);
-            
-        //     if($row>0)
-        //     {
-        //         $i =0;
-        //         foreach ($rows as $row)
-        //         {
-        //             $documents['Company']  = $row['Company'];
-        //             $documents['JobNum']  = $row['JobNum'];
-        //             $documents['ProdQty']  = $row['ProdQty'];
-        //             $documents['PaintMethod']  = $row['PaintMethod'];
-        //             $documents['RackStyle']  = $row['RackStyle'];
-        //             $documents['VehicleFamily']  = $row['VehicleFamily'];
-        //             $documents['PartNum']  = $row['PartNum'];
-        //             $documents['PartDescription']  = $row['PartDescription'];
-        //             $documents['DueDate']  = $row['DueDate'];
-        //             $documents['JobClosed']  = $row['JobClosed'];
-        //             $documents['JobComplete']  = $row['JobComplete'];
-        //             $documents['DateCreated']  = $row['DateCreated'];
-        //             $documents['DateModified']  = $row['DateModified'];
-                    
-        //             $result['data'][$i] = $documents;
-                
-        //             $i++;
-        //         }
-            
-        //         $mysqli->close();
-        //         generateServerResponse(S, SUCCESS,$result);
-        //     }
-        //     else
-        //     {
-        //         generateServerResponse(F, FAIL);
-        //     }
-            
-        //}
+            $result['data'][$i] = $documents;
+        
+            $i++;
+        }
+
+        generateServerResponse(S, SUCCESS,$result);
+        sqlsrv_free_stmt( $stmt);
+        sqlsrv_close($conn);
     }
 }
 ?>
